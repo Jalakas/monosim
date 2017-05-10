77ba45dfe102b709ff36a7b3cdbfd5019438f4eb
Jalakas <anari.jalakas@gmail.com>
2017-05-10 12:35:21 +0300

    More whitespace cleanup

--- monosim-gtk/Src/MainClass.cs
+++ monosim-gtk/Src/MainClass.cs
@@ -14,42 +14,42 @@ using monosimbase;
 
 namespace monosimgtk
 {
-	
+
 	public class MainClass
 	{
-		
-		
+
+
 		// Log4Net object
-        private static readonly ILog log = LogManager.GetLogger(typeof(monosimgtk.MainClass));
-		
-		
+		private static readonly ILog log = LogManager.GetLogger(typeof(monosimgtk.MainClass));
+
+
 		private static string retStr = "";
-		
+
 		public static string AppNameVer = "";
-		
-		
-		
+
+
+
 		#region Public Methods
-		
-		
-		
+
+
+
 		[STAThread]
 		public static void Main (string[] args)
 		{
 			AppNameVer = Assembly.GetExecutingAssembly().GetName().Name + " " +
-					     Assembly.GetExecutingAssembly().GetName().Version.ToString();
-			
+						 Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
 			// check for help request
 			if (new List<string>(args).Contains("--help"))
 			{
 				Console.WriteLine(GetHelpMsg());
 				return;
 			}
-			
+
 			Application.Init();
-			
+
 			retStr = GlobalObj.Initialize(args);
-			
+
 			// check for problems detected
 			if (retStr != "")
 			{
@@ -65,9 +65,9 @@ namespace monosimgtk
 					// warning (scard problem, can use serial reader)
 					ShowMessage(null, "WARNING", retStr, MessageType.Warning);
 				}
-				
+
 			}
-			
+
 			try
 			{
 				// try to set language
@@ -80,42 +80,42 @@ namespace monosimgtk
 				ShowMessage(null, "LANGUAGE SET ERROR", Ex.Message, MessageType.Error);
 				return;
 			}
-			
-			
+
+
 			// create new Gtk Gui for application and show it
 			MainWindowClass mwc = new MainWindowClass();
 			mwc.Show();
 			Application.Run ();
 		}
-		
-		
-		
-		
-		
+
+
+
+
+
 		/// <summary>
 		/// Show Message Window
 		/// </summary>
 		public static void ShowMessage(Gtk.Window parent, string title, string message, MessageType mt)
 		{
-			
+
 			// Gui Message
-			MessageDialog mdl = new MessageDialog(parent, 
-			                                      DialogFlags.DestroyWithParent, 
-			                                      mt, 
-			                                      ButtonsType.Ok,
-			                                      true,
-			                                      message);
+			MessageDialog mdl = new MessageDialog(parent,
+												  DialogFlags.DestroyWithParent,
+												  mt,
+												  ButtonsType.Ok,
+												  true,
+												  message);
 			mdl.Show();
 			mdl.Title = title;
-	        mdl.Icon = Gdk.Pixbuf.LoadFromResource("monosim.png");
-	        mdl.Run();
-			mdl.Destroy();                  
-	
+			mdl.Icon = Gdk.Pixbuf.LoadFromResource("monosim.png");
+			mdl.Run();
+			mdl.Destroy();
+
 		}
-		
-		
-		
-		
+
+
+
+
 		/// <summary>
 		/// Wait for pending GTK processes
 		/// </summary>
@@ -124,54 +124,54 @@ namespace monosimgtk
 			// Update GUI...
 			while (Gtk.Application.EventsPending ())
 			{
-            	Gtk.Application.RunIteration ();
+				Gtk.Application.RunIteration ();
 			}
 		}
-		
-		
-		
-		
-		
-		
+
+
+
+
+
+
 		#endregion Public Methods
-		
-		
-		
-		
-		
-		
+
+
+
+
+
+
 		#region Private Methods
-		
-		
-		
+
+
+
 		/// <summary>
 		/// Get help message to send to console
 		/// </summary>
 		private static string GetHelpMsg()
 		{
-			string msg = AppNameVer + " - GTK application to manage sim card contacts\r\n" + 
-				         GlobalObj.AppNameVer + " - base component\r\n\r\n";
+			string msg = AppNameVer + " - GTK application to manage sim card contacts\r\n" +
+						 GlobalObj.AppNameVer + " - base component\r\n\r\n";
 			msg += "   usage:\r\n";
 			msg += "   --log-console     enable log into console\r\n";
 			msg += "   --log-file        enable log into file comex.log into home folder\r\n";
 			msg += "   --help            show this message\r\n";
-			
+
 			return msg;
 		}
-		
-		
-		
-		
-		
-		
-		
+
+
+
+
+
+
+
 		#endregion Private Methods
-		
-		
-		
+
+
+
 	}
-	
-	
-	
-	
+
+
+
+
 }
--- monosim-qt/Src/MainClass.cs
+++ monosim-qt/Src/MainClass.cs
@@ -11,39 +11,39 @@ namespace monosimqt
 {
 	public class MainClass: Qt
 	{
-		
-		
+
+
 		// Log4Net object
-        private static readonly ILog log = LogManager.GetLogger(typeof(monosimqt.MainClass));
-		
+		private static readonly ILog log = LogManager.GetLogger(typeof(monosimqt.MainClass));
+
 		// Attributes
 		private static string retStr = "";
-		
-		
+
+
 		// Properties
 		public static string AppNameVer = "";
-		
-		
-		
+
+
+
 		public enum MessageType
 		{
 			Info=0,
 			Warning=1,
 			Error=2,
 		}
-		
-		
-		
+
+
+
 		#region Public Methods
-		
-		
-		
+
+
+
 		[STAThread]
 		public static void Main (string[] args)
 		{
 			AppNameVer = Assembly.GetExecutingAssembly().GetName().Name + " " +
-					     Assembly.GetExecutingAssembly().GetName().Version.ToString();
-			
+						 Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
 			// check for help request
 			if (new List<string>(args).Contains("--help"))
 			{
@@ -52,16 +52,16 @@ namespace monosimqt
 			}
 
 
-			// Init resource class manager			
+			// Init resource class manager
 			Q_INIT_RESOURCE("ResManager");
-			
-			
+
+
 			// Create new Qyoto Application
 			new QApplication(args);
 
-			
+
 			retStr = GlobalObj.Initialize(args);
-			
+
 			// check for problems detected
 			if (retStr != "")
 			{
@@ -70,7 +70,7 @@ namespace monosimqt
 				{
 					// error detected (not scard problem)
 					ShowMessage(null, "ERROR", retStr, MessageType.Error);
-					QApplication.Quit();					
+					QApplication.Quit();
 					return;
 				}
 				else
@@ -78,9 +78,9 @@ namespace monosimqt
 					// warning (scard problem, can use serial reader)
 					ShowMessage(null, "WARNING", retStr, MessageType.Warning);
 				}
-				
+
 			}
-			
+
 			try
 			{
 				// try to set language
@@ -93,32 +93,32 @@ namespace monosimqt
 				ShowMessage(null, "LANGUAGE SET ERROR", Ex.Message, MessageType.Error);
 				return;
 			}
-			
+
 			// Create new Qyoto Desktop Object
 			QDesktopWidget qdw = new QDesktopWidget();
-			
+
 			// Create MainWindow class manager
 			MainWindowClass mwc = new MainWindowClass();
-			
+
 			int wWidth = Convert.ToInt32(mwc.Width() / 2);
 			int wHeight = Convert.ToInt32(mwc.Height() / 2);
 			int dWidth = Convert.ToInt32(qdw.Width() / 2);
 			int dHeight = Convert.ToInt32(qdw.Height() / 2);
-			
+
 			mwc.Move(dWidth - wWidth, dHeight - wHeight - 20);
-			
+
 			mwc.Show();
-			
-			
+
+
 			// Run Qyoto Application
 			QApplication.Exec();
-			
+
 		}
-		
-		
-		
-		
-		
+
+
+
+
+
 		/// <summary>
 		/// Show Message Window
 		/// </summary>
@@ -136,13 +136,13 @@ namespace monosimqt
 			else if (mt == MainClass.MessageType.Error)
 			{
 				QMessageBox.Critical(parent, title, message);
-			}			
+			}
 		}
-		
-		
-		
-		
-		
+
+
+
+
+
 		/// <summary>
 		/// Wait for pending QT processes
 		/// </summary>
@@ -151,50 +151,50 @@ namespace monosimqt
 			// Update GUI...
 			QApplication.ProcessEvents();
 		}
-		
-		
-		
-		
-		
-		
+
+
+
+
+
+
 		#endregion Public Methods
-		
-		
-		
-		
-		
-		
+
+
+
+
+
+
 		#region Private Methods
-		
-		
-		
+
+
+
 		/// <summary>
 		/// Get help message to send to console
 		/// </summary>
 		private static string GetHelpMsg()
 		{
-			string msg = AppNameVer + " - Qt application to manage sim card contacts\r\n" + 
-				         GlobalObj.AppNameVer + " - base component\r\n\r\n";
+			string msg = AppNameVer + " - Qt application to manage sim card contacts\r\n" +
+						 GlobalObj.AppNameVer + " - base component\r\n\r\n";
 			msg += "   usage:\r\n";
 			msg += "   --log-console     enable log into console\r\n";
 			msg += "   --log-file        enable log into file comex.log into home folder\r\n";
 			msg += "   --help            show this message\r\n";
-			
+
 			return msg;
 		}
-		
-		
-		
-		
-		
-		
-		
+
+
+
+
+
+
+
 		#endregion Private Methods
-		
-		
-		
+
+
+
 	}
-	
+
 }
 
 
